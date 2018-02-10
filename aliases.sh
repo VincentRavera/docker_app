74c6042bf4f6a49a39fbe680bab3ee6157ce6d80
mobius <mobius@Dock.home>
2018-02-10 02:27:44 +0100

    First commit

---
+++ aliases.sh
@@ -0,0 +1,7 @@
+alias ipython="sh ~/.docker_app/jupyter-docker/Scripts/launch_ipython_shell.sh"
+
+alias jupyter="sh ~/.docker_app/jupyter-docker/Scripts/launch_jupyter.sh"
+
+alias youtube-dl="sh ~/.docker_app/youtube-dl-docker/launch_youtube-dl.sh"
+
+alias intellij="sh ~/.docker_app/intellij-docker/launch-intellij.sh"
