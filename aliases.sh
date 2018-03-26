c6b3d1a050414925a85130003a66d638ad11490d
Vincent <ravera.vincent@gmail.com>
2018-03-26 23:03:15 +0200

    docker cleanup

--- aliases.sh
+++ aliases.sh
@@ -7,3 +7,8 @@ alias youtube-dl="sh ~/.docker_app/youtube-dl-docker/launch_youtube-dl.sh"
 alias intellij="sh ~/.docker_app/intellij-docker/launch-intellij.sh"
 
 alias ipython-neural="sh ~/.docker_app/python-neural/Scripts/launch_ipython_neural.sh"
+
+# Docker clean up
+alias clean_docker_images='docker rmi $(docker images -a --filter=dangling=true -q)'
+alias clean_docker_containers='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
+
