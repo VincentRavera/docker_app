3b6c72568f6ec75647a516bb700c3d663ad14bf8
Vincent <ravera.vincent@gmail.com>
2018-09-12 00:59:10 +0200

    Add tor browser from phocean

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
+# My scripts
 alias ipython="sh ~/.docker_app/python-base/Scripts/launch_ipython_shell.sh"
 
 alias jupyter="sh ~/.docker_app/jupyter-docker/Scripts/launch_jupyter.sh"
@@ -8,7 +9,15 @@ alias intellij="sh ~/.docker_app/intellij-docker/launch-intellij.sh"
 
 alias ipython-neural="sh ~/.docker_app/python-neural/Scripts/launch_ipython_neural.sh"
 
+# Other Author
+
+# Tor Browser by phocean
+# https://github.com/phocean/dockerfile-torbrowser
+alias torbrowser="xhost +local:root && \
+    sudo docker run -i -t --rm -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro  -v \
+    /tmp/tor:/home/anon/Downloads:Z --device /dev/snd phocean/torbrowser && \
+    xhost -local:root"
+
 # Docker clean up
 alias clean_docker_images='docker rmi $(docker images -a --filter=dangling=true -q)'
 alias clean_docker_containers='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
-
--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ git clone https://github.com/VincentRavera/jupyter-docker
 git clone https://github.com/VincentRavera/python-base
 
 git clone https://github.com/VincentRavera/python-neural
+
+git clone https://github.com/phocean/dockerfile-torbrowser
+cd dockerfile-torbrowser
+docker build -t phocean/torbrowser .
+cd ..
